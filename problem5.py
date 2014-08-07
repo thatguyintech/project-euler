3317e985551a78e9d535ffd25bf2fd8ac68e1e74
Albert Hu <alberthu16@gmail.com>
2014-08-06 21:12:11 -0700

    solve problem5: 'smallest multiple' using Sieve of Eratosthenes, prime factorization helper, and a bunch of lists

--- problem5.py
+++ problem5.py
@@ -7,58 +7,39 @@ CORRECT ANSWER:
 232792560
 
 '''
-'''
-1 2 3 4 5 6 7 8 9 10
-2520
-
-19
-17
-13
-11
-7
-5
-3 .
-2 ...
-
-19*17*13*11*7*5*3*3*2*2*2*2
-
-20
-18
-16
-15
-14
-12
-10
-9
-8
-6
-4
-1
-'''
-
-from fractions import gcd
-
 def sieve(n):
     primes_list = [x for x in range(2, n+1)]
     composites_list = []
     for p in primes_list:
-        c = 1
-        while c*p < n:
+        c = 2
+        while c*p <= n:
             multiple = c*p
             if multiple in primes_list:
                 composites_list.append(primes_list.pop(primes_list.index(multiple)))
             c += 1
-    return (primes_list, composites_list) 
+    return primes_list, composites_list 
 
-def generate_evenly_divisible_product(n):
-    primes, comps = sieve(n)
-    result = 1
-    for p in primes:
-        result *= p
-    for c in comps:
-        if result % c != 0:
-            result *= gcd(result, c)
-    return result
-   
      
+def prime_factors(n):
+    primes, comps = sieve(n)
+    factors_list = list()
+    for prime in primes:
+        while n % prime == 0:
+            factors_list.append(prime)
+            n = n / prime 
+    return factors_list
 
+
+def prod(n):
+    prod_array = [[1]]*(n+1)
+    primes, composites = sieve(n)
+    for number in range(n, 1, -1):
+        factors = prime_factors(number)
+        for factor in factors:
+            group = [value for index, value in enumerate(factors) if value == factor]
+            if len(prod_array[factor]) <= len(group):
+                prod_array[factor] = list(group) 
+    total = 1
+    for index in range(len(prod_array)):
+        total *= prod_array[index][0]**len(prod_array[index])
+    return total
