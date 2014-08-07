168f43127231edfd6acda03b35c494e089fdd9f5
Albert Hu <alberthu16@gmail.com>
2014-08-06 17:49:13 -0700

    wokring on problem5.py, use an associative list to keep track of factors then multiply everything

--- problem5.py
+++ problem5.py
@@ -7,58 +7,70 @@ CORRECT ANSWER:
 232792560
 
 '''
-'''
-1 2 3 4 5 6 7 8 9 10
-2520
-
-19
-17
-13
-11
-7
-5
-3 .
-2 ...
-
-19*17*13*11*7*5*3*3*2*2*2*2
-
-20
-18
-16
-15
-14
-12
-10
-9
-8
-6
-4
-1
-'''
 
 from fractions import gcd
 
 def sieve(n):
     primes_list = [x for x in range(2, n+1)]
-    composites_list = []
+    composites_list = list() 
     for p in primes_list:
-        c = 1
-        while c*p < n:
+        c = 2
+        while c*p <= n:
             multiple = c*p
             if multiple in primes_list:
                 composites_list.append(primes_list.pop(primes_list.index(multiple)))
             c += 1
-    return (primes_list, composites_list) 
+    return primes_list, composites_list 
 
-def generate_evenly_divisible_product(n):
+def doit(n):
+    total = 1
+    arr = [[0]]*(n+1) 
+    for x in xrange(n, 1, -1):
+        comp_factors = prime_factors(x)
+        print "comp_factors: " +  comp_factors
+        for f in comp_factors: 
+            print [comp_factors.pop(comp_factors.index(num)) for num in comp_factors if num == f]
+            curr_prime_list = [comp_factors.pop(comp_factors.index(num)) for num in comp_factors if num == f]
+            if len(arr[f]) < len(curr_prime_list):
+               arr[f] = curr_prime_list 
+        print arr
+    for index in range(0, n+1):
+        total *= arr[index][0]*len(arr[index])
+    return total
+
+
+def get_index(n):
+    return max(prime_factors(n))
+
+def prod(n): 
     primes, comps = sieve(n)
+    primes_copy = list(primes)
     result = 1
+    for c in comps:
+        composites_factors = prime_factors(c)
+        for factor in composites_factors:
+            if factor not in primes_copy:
+                primes.append(factor)
+    print primes
     for p in primes:
         result *= p
+        print result
+    comps = sorted(comps, key=int, reverse=True)
+    print comps
     for c in comps:
-        if result % c != 0:
+        while result % c != 0:
+            print "{0} mod {1} = ".format(result, c) + str(result % c)
             result *= gcd(result, c)
+            comps.append(c)
+            print result
     return result
    
      
-
+def prime_factors(n):
+    primes, comps = sieve(n)
+    factor_list = list()
+    for p in primes:
+        while n % p == 0:
+            factor_list.append(p)
+            n = n/p
+    return factor_list
