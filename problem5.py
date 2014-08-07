9a30fb32e9009b2d9d71c942828634414feecb76
Albert Hu <alberthu16@gmail.com>
2014-08-06 21:23:38 -0700

    add documentation for problem 5, take out sieve() call in prod() cuz useless

--- problem5.py
+++ problem5.py
@@ -7,7 +7,45 @@ CORRECT ANSWER:
 232792560
 
 '''
+def prod(n):
+    """Generate the smallest positive number that is evenly divisible by all the
+    numbers from 1 to n.
+
+    The idea I'm going for is: 
+
+    - iterate from n to 1, backwards
+    - find the prime factors of each n and keep track of the most instances of each
+      prime factor
+    - multiple the count together at the end
+    """ 
+    prod_array = [[1]]*(n+1)
+    for number in range(n, 1, -1):
+        factors = prime_factors(number)
+        for factor in factors:
+            group = [value for index, value in enumerate(factors) if value == factor]
+            if len(prod_array[factor]) <= len(group):
+                prod_array[factor] = list(group) 
+    total = 1
+    for index in range(len(prod_array)):
+        total *= prod_array[index][0]**len(prod_array[index])
+    return total
+
+
+def prime_factors(n):
+    """Returns the prime factorization of n in a list.
+    """
+    primes, comps = sieve(n)
+    factors_list = list()
+    for prime in primes:
+        while n % prime == 0:
+            factors_list.append(prime)
+            n = n / prime 
+    return factors_list
+
+
 def sieve(n):
+    """This function I learned how to write just for fun. Totally useless.
+    """
     primes_list = [x for x in range(2, n+1)]
     composites_list = []
     for p in primes_list:
@@ -19,27 +57,3 @@ def sieve(n):
             c += 1
     return primes_list, composites_list 
 
-     
-def prime_factors(n):
-    primes, comps = sieve(n)
-    factors_list = list()
-    for prime in primes:
-        while n % prime == 0:
-            factors_list.append(prime)
-            n = n / prime 
-    return factors_list
-
-
-def prod(n):
-    prod_array = [[1]]*(n+1)
-    primes, composites = sieve(n)
-    for number in range(n, 1, -1):
-        factors = prime_factors(number)
-        for factor in factors:
-            group = [value for index, value in enumerate(factors) if value == factor]
-            if len(prod_array[factor]) <= len(group):
-                prod_array[factor] = list(group) 
-    total = 1
-    for index in range(len(prod_array)):
-        total *= prod_array[index][0]**len(prod_array[index])
-    return total
